b8ba1cca9d63765ca83bbc76af6689f3ba3c3768
JollyFoxy <pavel.mizirev@gmail.com>
2022-01-21 14:57:00 +0300

    работа с утками

---
+++ ConsoleApp/ClassLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+   abstract public class Duck
+    {
+        public string Quack()
+        {
+            return "Quack " + this.GetType();
+        }
+
+        public string Swim()
+        {
+            return "Swim " + this.GetType();
+        }
+
+        abstract public string Display();
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck(); // экземпляр класса MallarDuck
+            Duck redheadDuck = new RedheadDuck(); // экземпляр класса RedheadDuck
+
+            Duck[] ducks = new Duck[] { mallardDuck, redheadDuck }; // Массив уток с типом данных Duck
+
+            for (int i = 0; i < ducks.Length; i++) 
+            {
+                Console.WriteLine(ducks[i].Display());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Quack());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
